c22670815d806988e9b537500c7be7869cf8a241
konstant_ks <knitnatsnoks@gmail.com>
2023-08-31 00:29:42 +0300

    Task 2 correct solution

--- src/Fintech/Task2.java
+++ src/Fintech/Task2.java
@@ -2,21 +2,22 @@ package Fintech;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
 import java.util.StringJoiner;
 
 public class Task2 {
     public static void main(String[] args) {
-//        Scanner scanner = new Scanner(System.in);
+        Scanner scanner = new Scanner(System.in);
 
-        int n = 3;
-        int m = 3;
-//        if (scanner.hasNext()) {
-//            n = scanner.nextInt();
-//        }
-//        if (scanner.hasNext()) {
-//            m = scanner.nextInt();
-//        }
-        int[][] matrix = new int[n][m];
+        int n = 1;
+        int m = 1;
+        if (scanner.hasNext()) {
+            n = scanner.nextInt();
+        }
+        if (scanner.hasNext()) {
+            m = scanner.nextInt();
+        }
+        int[][] matrix = new int[m][n];
 
         int maxValue = n * m;
         int count = 1;
@@ -26,32 +27,32 @@ public class Task2 {
         boolean desc = false;
         while (count <= maxValue) {
             if (!desc) {
-                for (int i = baseN, j = 0; i >= 0 && j <= baseM; ) {
+                for (int i = 0, j = baseN; i <= baseM && j >= 0; ) {
                     matrix[i][j] = count++;
-                    i--;
-                    j++;
-                    if (count > maxValue) {
+                    i++;
+                    j--;
+                    if (count >= maxValue) {
                         break;
                     }
                 }
             } else {
-                for (int i = baseN, j = descBaseM; i >= 0 && j <= baseM; ) {
+                for (int i = descBaseM, j = baseN; i <= baseM && j >= 0; ) {
                     matrix[i][j] = count++;
-                    i--;
-                    j++;
-                    if (count > maxValue) {
+                    i++;
+                    j--;
+                    if (count >= maxValue) {
                         break;
                     }
                 }
             }
-            if (baseN < n -1) {
+            if (baseN < n - 1) {
                 baseN++;
-            }
-            if (baseM < m - 1) {
-                baseM++;
             } else {
                 desc = true;
                 descBaseM++;
+            }
+            if (baseM < m - 1) {
+                baseM++;
             }
 
         }
@@ -65,6 +66,5 @@ public class Task2 {
             list.add(stringJoiner.toString());
         }
         System.out.println(String.join("\n", list));
-//        System.out.println(Arrays.deepToString(matrix));
     }
 }
